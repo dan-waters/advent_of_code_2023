c954a82214182d5d51a215d1ad406abe417c0441
Daniel Waters <daniel.waters@softwire.com>
2023-12-09 20:43:07 +0000

    day 9 some more interesting solutions

--- day9/main.rb
+++ day9/main.rb
@@ -4,21 +4,19 @@ lines = open('input.txt').readlines(chomp: true)
 
 Sequence = Struct.new(:numbers) do
   def expansion
-    @differences ||= begin
-                       @differences = [numbers]
-                       until @differences.last.all? { |x| x == 0 } do
-                         new_differences = []
-                         @differences.last.each_cons(2) do |pair|
-                           new_differences << pair[1] - pair[0]
-                         end
-                         @differences << new_differences
-                       end
-                       @differences
-                     end
+    differences = [numbers]
+    until differences.last.all? { |x| x == 0 } do
+      new_differences = []
+      differences.last.each_cons(2) do |pair|
+        new_differences << pair[1] - pair[0]
+      end
+      differences << new_differences
+    end
+    differences
   end
 
   def prediction
-    rows = expansion.reverse.map {|a| a.dup}
+    rows = expansion.reverse.map { |a| a.dup }
     rows.each_cons(2) do |previous_row, next_row|
       next_row << next_row.last + previous_row.last
     end
@@ -26,7 +24,7 @@ Sequence = Struct.new(:numbers) do
   end
 
   def back_prediction
-    rows = expansion.reverse.map {|a| a.dup}
+    rows = expansion.reverse.map { |a| a.dup }
     rows.each_cons(2) do |previous_row, next_row|
       next_row.unshift(next_row.first - previous_row.first)
     end
@@ -41,3 +39,21 @@ end
 
 puts sequences.map(&:prediction).sum
 puts sequences.map(&:back_prediction).sum
+
+# other solutions - part 2 with reverse
+
+sequences = lines.map do |line|
+  numbers = line.split.map(&:to_i)
+  Sequence.new(numbers.reverse)
+end
+puts "reverse: #{sequences.map(&:prediction).sum}"
+
+# other solutions - adding up like pascal's triangle
+
+sequences = lines.map do |line|
+  numbers = line.split.map(&:to_i)
+  Sequence.new(numbers)
+end
+
+puts "pascal part 1: #{sequences.map(&:expansion).map { |a| a.map { |a2| a2.last }.sum }.sum}"
+puts "pascal part 2: #{sequences.map(&:expansion).map { |a| a.map { |a2| a2.first }.reverse.inject(0) { |sum, x| -(sum - x) } }.sum}"
